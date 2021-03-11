2d510ac978925f76e5a83ebfd4c90611440ba95c
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2021-03-11 05:24:53 -0300

    updating

--- src/components/BalanceBar/index.js
+++ src/components/BalanceBar/index.js
@@ -1,23 +1,64 @@
 import React from 'react'
 import logoImg from '../../assets/BalanceBar/Logos.svg'
+import caretDown from '../../assets/BalanceBar/CaretDown.svg'
+import eyeImg from '../../assets/BalanceBar/eye.svg'
+import copy from '../../assets/BalanceBar/CopySimple.svg'
 import {
-    Header,
+    Container,
+    BalanceArea,
     Separator,
-    Logo,
+    Img,
     Account,
-    
+    NameAccount,
+    NumberAccount,
+    NumberOne,
+    NumberTwo,
+    Balance,
+    Saldo,
+    InputArea,
+    Input,
+
 } from './styles'
 
 
 export default function BalanceBar(){
    return(
     <>   
-    <Header >
-        <Logo logo={logoImg}/>
+    <Container >
+        <BalanceArea>
+        <Img img={logoImg} width={130} height={21}/>
+        
         <Separator/>
-        <Account/>
+        
+        <Account>
+            <NameAccount size={16}>
+                Conta Corrente
+                <Img img={caretDown} width={16} height={16}/>
+            </NameAccount>
 
-    </Header>
+            <NumberAccount>
+                <NumberOne>Ag. 00007-1</NumberOne>  
+                
+                <NumberTwo>C.C. 00000000000000000020-1</NumberTwo>
+
+                <Img img={copy} width={16} height={16}/>
+            </NumberAccount>
+        </Account>
+
+        <Balance>
+            <NameAccount size={12}>Saldo <Img img={eyeImg} width={16} height={16}/></NameAccount>
+
+            <Saldo>R$40.000.00</Saldo>
+        </Balance>
+        </BalanceArea>
+
+        <InputArea>
+            <Input></Input>
+        </InputArea>
+    
+    </Container>
+
+
    </>
    )
 }
--- src/components/BalanceBar/styles.js
+++ src/components/BalanceBar/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Header = styled.div`
+export const Container = styled.div`
 grid-area:"BB";
 grid-column-start:1;
 grid-column-end:-1;
@@ -9,21 +9,27 @@ display:flex;
 flex-direction:row;
 align-items:center;
 
-position:relative;
-
-padding:25px 20px 25px 15px;
+padding:0 20px;
 `;
 
-export const Logo = styled.div`
+export const BalanceArea = styled.div`
+display:flex;
+flex-direction:row;
+align-items:center;
+`
 
-background:url(${p => p.logo});
 
-width:130px;
-height:21px;
+export const Img = styled.div`
+
+background:url(${p => p.img});
+background-repeat:no-repeat;
+
+width:${p => p.width}px;
+height:${p => p.height}px;
 
 margin-right:5px;
 
-
+cursor:pointer;
 `;
 
 
@@ -32,6 +38,71 @@ width:1px;
 height:25px;
 
 border:solid 1px #E5E4E2;
+border-radius:10px;
 
-`
+`;
 
+
+export const Account = styled.div`
+width:18rem;
+display:flex;
+flex-direction:column;
+
+margin-left:20px;
+`;
+
+
+export const NameAccount = styled.div`
+display:flex;
+justify-self:flex-start;
+align-items:center;
+
+margin-bottom:5px;
+
+color:var(--text-color);
+font-size:${p => p.size}px;
+
+`;
+
+export 
+
+const NumberAccount = styled.div`
+display:flex;
+justify-content:space-between;
+
+color:var(--primary);
+font-size:12px;
+`;
+
+
+export const NumberOne = styled.div`
+`;
+
+export const NumberTwo = styled.div`
+`;
+
+export const Balance = styled.div`
+ display:flex;
+ flex-direction:column;
+
+ margin-left:25px;
+`;
+
+export const Saldo = styled.text`
+color:var(--secondary);
+`;
+
+
+export const InputArea = styled.div`
+display:flex;
+justify-self:flex-end;
+background-color:green;
+
+width:200px;
+
+`;
+
+
+export const Input = styled.input`
+
+`;
